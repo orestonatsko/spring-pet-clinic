e0c295e723c726cd389971845ff65fc5e7f0184a
Orest Onatsko <orestonatsko@gmail.com>
2019-02-09 18:47:00 +0200

    closed #2

--- pet-clinic-data/src/main/java/com/orestonatsko/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/orestonatsko/springpetclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package com.orestonatsko.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Set> pets;
+
+    public Set<Set> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Set> pets) {
+        this.pets = pets;
+    }
 }
