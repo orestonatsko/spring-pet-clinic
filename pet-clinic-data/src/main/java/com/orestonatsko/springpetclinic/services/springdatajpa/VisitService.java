d3b23c7ad174924115a5599767b59b9b1475e4a3
Orest Onatsko <orestonatsko@gmail.com>
2019-03-07 15:01:30 +0200

    Implemented Map impl for Visit Service closes#22

---
+++ pet-clinic-data/src/main/java/com/orestonatsko/springpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package com.orestonatsko.springpetclinic.services.springdatajpa;
+
+import com.orestonatsko.springpetclinic.model.Visit;
+import com.orestonatsko.springpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
