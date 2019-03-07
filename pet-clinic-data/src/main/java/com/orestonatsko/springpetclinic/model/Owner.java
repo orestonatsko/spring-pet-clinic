e8415e243fa379a2780a4e3e72b2dafb12d48793
Orest Onatsko <orestonatsko@gmail.com>
2019-03-07 12:29:23 +0200

    closes #12

--- pet-clinic-data/src/main/java/com/orestonatsko/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/orestonatsko/springpetclinic/model/Owner.java
@@ -1,15 +1,24 @@
 package com.orestonatsko.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owner")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
-    private String city;
-    private String telephone;
-    private Set<Pet> pets = new HashSet<>();
 
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
